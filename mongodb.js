d9fa0af900e0a583d111d5ccff447dd0b622bad4
Pawan Gawande <ppgawande12@gmail.com>
2024-07-24 00:16:01 +0530

    added mongobd

---
+++ mongodb.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URL;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+}
+
+module.exports = { run, client };
